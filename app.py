cda6c1df6968fda6c54b7c4c86665932e78066db
201st-Luka <luka.eichten@gmail.com>
2023-09-18 21:40:48 +0200

    feat: web ui

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from os import environ
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+app.debug = environ.get("DEBUG")
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
