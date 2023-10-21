11d3e825d521471f42521672ea2999be4d0a6f71
201st-Luka <luka.eichten@gmail.com>
2023-10-21 09:43:09 +0200

    simulate button is blocked if simulation is not over

--- static/js/requests/simulator.js
+++ static/js/requests/simulator.js
@@ -1,56 +1,69 @@
+// paging variables
+// rowCount is used to display only 2000 rows on the table
 const rowCount = 2000;
+// page is used to count the pages and show the rows from `(page - 1) * rowCount` to `page * rowCount`
 let page = 1;
 
 
-const combinationsS = () => document.getElementById('combinations-s');
-const combinationsP = () => document.getElementById('combinations-p');
-const combinationsD = () => document.getElementById('combinations-d');
-const combinationsF = () => document.getElementById('combinations-f');
-const combinationsTotal = () => document.getElementById('combinations-total');
+// constants of the HTML elements that are modified or used by the simulator
+const combinationsS = document.getElementById('combinations-s');
+const combinationsP = document.getElementById('combinations-p');
+const combinationsD = document.getElementById('combinations-d');
+const combinationsF = document.getElementById('combinations-f');
+const combinationsTotal = document.getElementById('combinations-total');
 
-const simulateButton = () => document.getElementById('sim-input-simulate');
+const simulateButton = document.getElementById('sim-input-simulate');
 
-const inputForm = () => document.getElementById('sim-input-form');
+const inputForm = document.getElementById('sim-input-form');
 
-const simulatorIntputS = () => document.getElementById('sim-input-s');
-const simulatorIntputP = () => document.getElementById('sim-input-p');
-const simulatorIntputD = () => document.getElementById('sim-input-d');
-const simulatorIntputF = () => document.getElementById('sim-input-f');
+const simulatorIntputS = document.getElementById('sim-input-s');
+const simulatorIntputP = document.getElementById('sim-input-p');
+const simulatorIntputD = document.getElementById('sim-input-d');
+const simulatorIntputF = document.getElementById('sim-input-f');
 
-const rowsTable = () => document.getElementById('rows-table');
-const termsTable = () => document.getElementById('group-table');
+const rowsTable = document.getElementById('rows-table');
+const termsTable = document.getElementById('group-table');
 
-const pagePrevButton = () => document.getElementById('page-button-prev');
-const pageNextButton = () => document.getElementById('page-button-next');
-const pageLabel = () => document.getElementById('page-label');
+const pagePrevButton = document.getElementById('page-button-prev');
+const pageNextButton = document.getElementById('page-button-next');
+const pageLabel = document.getElementById('page-label');
 
 
+// functions
+/*
+ * float_to_fraction is used to build a nice fraction of the floating point number of the term instead of displaying the
+ * ugly floating point number
+ */
 function float_to_fraction(value) {
     if (Math.floor(value) === value) {
         return `${value}`;
     } else {
-        return `<label class="fraction"><sup>${value * 2}</sup>&frasl;<sub>2</sub></label>`
+        return `<label class="fraction"><sub><sup>${value * 2}</sup>&frasl;<sub>2</sub></sub></label>`
     }
 }
 
-
+/*
+ * core function of the web page
+ * this function executes the request to the simulator API and displays it
+ */
 function simulate() {
-    simulateButton().disable;
-    pagePrevButton().disabled = true;
-    pageNextButton().disabled = true;
+    // modifying the buttons so they cannot be pressed again
+    simulateButton.disabled = true;
+    pagePrevButton.disabled = true;
+    pageNextButton.disabled = true;
+    simulateButton.value = "Simulating ...";
 
-    page = 1;
-
+    // fetching the request
     fetch('/api/simulator', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ data: {
-            s: simulatorIntputS().value,
-            p: simulatorIntputP().value,
-            d: simulatorIntputD().value,
-            f: simulatorIntputF().value,
+            s: simulatorIntputS.value,
+            p: simulatorIntputP.value,
+            d: simulatorIntputD.value,
+            f: simulatorIntputF.value,
             range_start: rowCount * (page - 1),
             range_end: rowCount * page
         } }),
@@ -59,38 +72,48 @@ function simulate() {
     .then(data => {
         const total_combs = data.s * data.p * data.d * data.f;
 
-        combinationsS().innerText = `${data.s}`;
-        combinationsP().innerText = `${data.p}`;
-        combinationsD().innerText = `${data.d}`;
-        combinationsF().innerText = `${data.f}`;
-        combinationsTotal().innerText = `${total_combs}`;
+        // displaying the combinations
+        combinationsS.innerText = `${data.s}`;
+        combinationsP.innerText = `${data.p}`;
+        combinationsD.innerText = `${data.d}`;
+        combinationsF.innerText = `${data.f}`;
+        combinationsTotal.innerText = `${total_combs}`;
 
+        // enabling the page buttons if possible (if the next or previous page is not out of range)
         if (page > 1) {
-            pagePrevButton().disabled = false;
+            pagePrevButton.disabled = false;
         }
         if (rowCount * page < total_combs) {
-            pageNextButton().disabled = false;
+            pageNextButton.disabled = false;
         }
 
-        while (rowsTable().rows.length > 3) {
-            rowsTable().deleteRow(3);
+        // deleting the rows that where showed previously on the tables
+        while (rowsTable.rows.length > 3) {
+            rowsTable.deleteRow(3);
         }
-        while (termsTable().rows.length > 2) {
-            termsTable().deleteRow(2);
+        while (termsTable.rows.length > 2) {
+            termsTable.deleteRow(2);
         }
 
+        // displaying the new configuration
         for (let i = 0; i < data.rows.length; ++i) {
-            const tr = rowsTable().insertRow();
+            // creating a new row
+            const tr = rowsTable.insertRow();
+            // creating a new cell of the row
             const index = tr.insertCell()
+            // displaying the index of the row
             index.innerText = `${data.range_start + i + 1}`
             for (let j = 0; j < 18; ++j) {
+                // creating a new cell for the arrows and ml/ms values
                 const td = tr.insertCell();
+                // displaying the arrow or ml/ms value
                 td.innerText = `${data.rows[i][j]}`;
             }
         }
 
+        // displaying the new terms (same code as for the configuration)
         for (let i = 0; i < data.terms.length; ++i) {
-            const tr = termsTable().insertRow();
+            const tr = termsTable.insertRow();
             for (let j = 0; j < 4; ++j) {
                 const td = tr.insertCell();
                 td.innerText = `${data.terms[i][j]}`;
@@ -102,45 +125,67 @@ function simulate() {
             }
             td.innerHTML = htmlArray.join(', ');
         }
+
+        // enabling the button again and setting its value to indicate the simulation if over
+        simulateButton.disabled = false;
+        simulateButton.value = "Simulate";
     })
     .catch(error => {
+        // error handling
         console.error('Error:', error);
+
+        // enabling the button again and setting its value to indicate the simulation if over
+        simulateButton.disabled = false;
+        simulateButton.innerText = "Simulate";
     });
-    simulateButton().disabled = false;
 }
 
+/*
+ * render the previous page
+ */
 function prev_page() {
     page -= 1;
-    pageLabel().innerText = `${page}`;
+    pageLabel.innerText = `${page}`;
     simulate();
 }
 
+/*
+ * render the next page
+ */
 function next_page() {
     page += 1;
-    pageLabel().innerText = `${page}`;
+    pageLabel.innerText = `${page}`;
     simulate();
 }
 
+/*
+ * submit the input form and prevent the default event from happening
+ */
 function submit_input(event) {
     event.preventDefault();
     simulate();
 }
 
+/*
+ * connecting the buttons and callbacks
+ */
 function doc_loading() {
-    simulateButton().disabled = true;
-    pagePrevButton().disabled = true;
-    pageNextButton().disabled = true;
+    // resetting the page to 1
+    page = 1;
 
-    // simulateButton().addEventListener('click', simulate);
+    // disabling the buttons
+    simulateButton.disabled = true;
+    pagePrevButton.disabled = true;
+    pageNextButton.disabled = true;
 
-    pagePrevButton().addEventListener('click', prev_page);
-    pageNextButton().addEventListener('click', next_page);
+    // connecting the buttons and its callback functions
+    pagePrevButton.addEventListener('click', prev_page);
+    pageNextButton.addEventListener('click', next_page);
+    inputForm.addEventListener('submit', submit_input);
 
-    inputForm().addEventListener('submit', submit_input);
-
+    // starting the simulator once
     simulate();
-    simulateButton().disabled = false;
 }
 
-
+// calling the `doc_loading` function once if the document is loaded (if the web page is opened or reloaded)
 document.addEventListener('DOMContentLoaded', doc_loading);
